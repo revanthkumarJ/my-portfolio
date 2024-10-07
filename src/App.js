3189b2b75c73db2335aca109f77ce4e51b0a4eb3
revanthkumarJ <jrevanth101@gmail.com>
2024-10-07 11:03:41 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/my-portfolio" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/certifications" element={<Certifications />} />
         <Route path="/about" element={<About />} />
